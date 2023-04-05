c1ae9aa9e61b9495fad26e3dfc971fac1ef14a81
alexmquan <alexmquan93@gmail.com>
2023-04-05 12:12:59 -0600

    Built basic skeleton of site

---
+++ Tower.client/src/models/TowerEvent.js
@@ -0,0 +1,19 @@
+import { Profile } from "./Account.js"
+
+
+export class TowerEvent {
+  constructor(data) {
+    this.id = data.id
+    this.name = data.name
+    this.description = data.description
+    this.coverImg = data.coverImg
+    this.location = data.location
+    this.capacity = data.capacity
+    this.startDate = data.startDate
+    this.isCanceled = data.isCanceled
+    this.type = data.type
+    this.creatorId = data.creatorId
+    this.creator = data.creator ? new Profile(data.creator) : null
+
+  }
+}
--- Tower.client/src/pages/HomePage.vue
+++ Tower.client/src/pages/HomePage.vue
@@ -1,44 +1,37 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img
-        src="https://bcw.blob.core.windows.net/public/img/8600856373152463"
-        alt="CodeWorks Logo"
-        class="rounded-circle"
-      >
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
-    </div>
+  <!-- SECTION Home Page Image With text over -->
+  <div class="container-fluid">
+    <section class="row">
+      <div class="col-12">
+        <div></div>
+      </div>
+    </section>
+    <!-- SECTION Sort Bar -->
+    <section>
+
+    </section>
+    <!-- SECTION Sort Bar -->
+    <section></section>
+    <!-- SECTION Event Cards -->
+    <section class="row">
+      <div class="col-md-3 col-12" v-for="">
+
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import { onMounted, computed, ref } from 'vue';
+import { AppState } from "../AppState.js";
+
 export default {
   setup() {
-    return {}
+    return {
+      events: computed(() => AppState.towerEvent)
+    }
   }
 }
 </script>
 
-<style scoped lang="scss">
-.home {
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
-
-  .home-card {
-    width: 50vw;
-
-    >img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
-}
-</style>
+<style scoped lang="scss"></style>
