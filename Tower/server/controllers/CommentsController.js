284cf29981525e7bd6e6610e2284b2dd69fecf82
alexmquan <alexmquan93@gmail.com>
2023-04-05 11:21:21 -0600

    Create comment and get all comments completed and tested

--- Tower/server/controllers/CommentsController.js
+++ Tower/server/controllers/CommentsController.js
@@ -8,13 +8,26 @@ export class CommentsController extends BaseController {
     this.router
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.createComment)
+      .delete('/:id', this.deleteComment)
   }
 
   async createComment(req, res, next) {
     try {
       const commentData = req.body
-      commentData.accountId = req.userInfo.id
+      commentData.creatorId = req.userInfo.id
       const comment = await commentsService.createComment(commentData)
+      return res.send(comment)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async deleteComment(req, res, next) {
+    try {
+      const commentId = req.params.id
+      const userId = req.userInfo.id
+      let message = 
+
     } catch (error) {
       next(error)
     }
